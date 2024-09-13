073fee31b2b3bf5b27abfe070d5cee196eb3f80d
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-09-13 12:38:42 -0500

    hf removed

--- src/components/Habitaciones.jsx
+++ src/components/Habitaciones.jsx
@@ -17,7 +17,7 @@ const imagesWithTitles = [
 
 const Hotels = () => {
     return (
-        <div className="h-screen bg-white text-black">
+        <div className="bg-white text-black">
             <div className="flex items-center max-w-[986px] mx-auto px-5 py-20 font-bold">
                 <div>
                     <h1 className='mb-3 big-responsive-text text-orange-500'>Mas que habitaciones, experiencias.</h1>
