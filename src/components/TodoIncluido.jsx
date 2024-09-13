399b69d2c107220683544cd545faf206ee02e5b0
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-09-13 14:34:23 -0500

    Todo Incluido

--- src/components/TodoIncluido.jsx
+++ src/components/TodoIncluido.jsx
@@ -1,10 +1,26 @@
+import image from '../assets/todo-incluido.png';
+
 const TodoIncluido = () => {
     return (
-        <div className="bg-white text-black">
-            <div className="flex items-center max-w-[986px] mx-auto px-5 py-20 font-bold">
-                <div>
-                    <h1 className='mb-3 big-responsive-text text-orange-500'>Todo Incluido</h1>
-                    <p className='medium-responsive-text'>Todo Incluido</p>
+        <div className="bg-black text-white relative">
+            <div className="grid grid-cols-2 gap-4 max-w-[986px] mx-auto px-5 py-20">
+                <div className="col-span-1 font-bold flex flex-col justify-center">
+                    <div className='text-center space-y-8'>
+                        <h1 className='mb-3 big-responsive-text text-orange-500'>Todo Incluido</h1>
+                        <p className='small-responsive-text'>
+                        La experiencia Gourmet Inclusive® garantiza un servicio de cinco estrellas <br></br> para todos los huéspedes. <br></br>
+                        Disfruta de 6 restaurantes, bebidas premium y servicio personalizado.
+                        </p>
+                        <ul className='list-none space-x-3 medium-responsive-text'>
+                            <li className='text-orange-500 inline-block'><i className='fas fa-burger'></i></li>
+                            <li className='text-orange-500 inline-block'><i className='fas fa-utensils'></i></li>
+                            <li className='text-orange-500 inline-block'><i className='fas fa-cocktail'></i></li>
+                            <li className='text-orange-500 inline-block'><i className='fas fa-car'></i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <img src={image} />
                 </div>
             </div>
         </div>
