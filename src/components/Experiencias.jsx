c1d8b40a7ea5908b25ea004b2fd26c413d960eb7
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-09-13 13:35:05 -0500

    Componente: Experiencias para todos

--- src/components/Experiencias.jsx
+++ src/components/Experiencias.jsx
@@ -1,10 +1,36 @@
+import experiencias1 from '../assets/experiencias-1.png';
+import experiencias2 from '../assets/experiencias-2.png';
+import experiencias3 from '../assets/experiencias-3.png';
+
 const Experiencias = () => {
     return (
-        <div className="bg-white text-black">
-            <div className="flex items-center max-w-[986px] mx-auto px-5 py-20 font-bold">
-                <div>
-                    <h1 className='mb-3 big-responsive-text text-orange-500'>Experiencias</h1>
-                    <p className='medium-responsive-text'>experiencias</p>
+        <div className="bg-white text-black py-20">
+            <div className="flex items-center max-w-[986px] mx-auto px-5 pb-20 font-bold">
+                <div className="m-auto">
+                    <h1 className='mb-3 big-responsive-text'>Experiencias para todos</h1>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-[986px] mx-auto px-5 text-center">
+                <div className="p-5">
+                    <img src={experiencias1} className='m-auto pb-4' />
+                    <h2 className="small-responsive-text font-bold mb-2">
+                        Club para pequeños
+                    </h2>
+                    <p>Sus pequeños estarán felices jugando sin parar, haciendo nuevos amigos, escuchando nuevos cuentos y más.</p>
+                </div>
+                <div className="p-5">
+                    <img src={experiencias2} className='m-auto pb-4' />
+                    <h2 className="small-responsive-text font-bold mb-2">
+                        Atracciones para todos
+                    </h2>
+                    <p>Las diversiones para todas las edades en Nick no tienen fin como Nickelodeon Place, Aqua Nick y Plaza Orange.</p>
+                </div>
+                <div className="p-5">
+                    <img src={experiencias3} className='m-auto pb-4' />
+                    <h2 className="small-responsive-text font-bold mb-2">
+                        Spa y gimnasio
+                    </h2>
+                    <p>Deléitese con servicios de masajes y tratamientos faciales, así como con equipos de ejercicio modernos,</p>
                 </div>
             </div>
         </div>
