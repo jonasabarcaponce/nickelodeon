57f8c157594790e041b4f5463b33522857743c6d
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-10-21 09:10:36 -0500

    cabecera al 100%

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,9 +1,20 @@
-import bgImage from '../assets/bg-2.jpg';
 import logoImage from '../assets/logo.png';
 import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// Slider data
+import image1 from '../assets/bg-2.jpg';
+import image2 from '../assets/bg.png';
+
+const imagesWithTitles = [
+    { src: image1, title: 'Disfruta tu vacaciones como nunca', subtitle: 'Junto a Bob Esponja, Las Tortugas Ninja y muchos otros personajes de Nick en una escapada inolvidable.' },
+    { src: image2, title: '¡Ahorra hasta un 30% de descuento!', subtitle: 'Reserva ahora con nosotros para obtener todos los beneficios' },
+];
 
 // Reusable Sidenav Component
-// eslint-disable-next-line react/prop-types
 const NavegacionLateral = ({ isOpen, toggleSideNav }) => {
     return (
         <>
@@ -24,7 +35,7 @@ const NavegacionLateral = ({ isOpen, toggleSideNav }) => {
                     onClick={toggleSideNav}
                     className="absolute top-4 right-4 text-white focus:outline-none"
                 >
-                    <i className='fas fa-times'></i>
+                    <i className="fas fa-times"></i>
                 </button>
                 <div className="py-10 px-5">
                     <ul className="space-y-5">
@@ -102,40 +113,46 @@ const Cabecera = () => {
         <>
             <NavegacionLateral isOpen={isOpen} toggleSideNav={toggleSideNav} />
             {/* Main Header Section */}
-            <div
-                className="relative h-screen bg-no-repeat bg-cover bg-right sm-bg-center"
-                style={{ backgroundImage: `url(${bgImage})` }}
+            <Swiper
+                modules={[Autoplay]}
+                autoplay={{
+                    delay: 3000, // Delay between transitions in milliseconds
+                    disableOnInteraction: true, // Keeps autoplay running even after interaction
+                }}
+                loop={true}
             >
-                {/* Black Overlay */}
-                <div className="absolute inset-0 bg-black opacity-50"></div>
-                <div className="relative max-w-[986px] mx-auto py-10 px-5">
-                    <nav className="flex justify-between items-center">
-                        <div>
-                            <img src={logoImage} className="w-6/12 md:w-8/12" alt="Logo" />
+                {imagesWithTitles.map((item, index) => (
+                    <SwiperSlide
+                        key={index}
+                        className="relative h-screen bg-no-repeat bg-cover bg-right sm-bg-center"
+                        style={{ backgroundImage: `url(${item.src})` }}
+                    >
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
+                        <div className="relative max-w-[986px] mx-auto py-10 px-5">
+                            <nav className="flex justify-between items-center">
+                                <div>
+                                    <img src={logoImage} className="w-6/12 md:w-8/12" alt="Logo" />
+                                </div>
+                                <div className="flex flex-row space-x-2">
+                                    <a
+                                        href="#"
+                                        className="bg-black text-xl text-white flex items-center justify-center rounded-full min-w-12 w-12 h-12"
+                                        onClick={toggleSideNav}
+                                    >
+                                        <i className="fas fa-bars"></i>
+                                    </a>
+                                </div>
+                            </nav>
                         </div>
-                        <div className="flex flex-row space-x-2">
-                            <a
-                                href="#"
-                                className="bg-black text-xl text-white flex items-center justify-center rounded-full min-w-12 w-12 h-12"
-                                onClick={toggleSideNav}
-                            >
-                                <i className="fas fa-bars"></i>
-                            </a>
+                        <div className="relative flex items-center max-w-[986px] mx-auto px-5 h-4/6 font-bold text-white">
+                            <div>
+                                <h1 className="mb-3 big-responsive-text">{item.title}</h1>
+                                <p className="medium-responsive-text">{item.subtitle}</p>
+                            </div>
                         </div>
-                    </nav>
-                </div>
-                <div className="relative flex items-center max-w-[986px] mx-auto px-5 h-4/6 font-bold text-white">
-                    <div>
-                        <h1 className="mb-3 big-responsive-text">
-                            Disfruta tu vacaciones como nunca
-                        </h1>
-                        <p className="medium-responsive-text">
-                            Junto a Bob Esponja, Las Tortugas Ninja y muchos otros personajes
-                            de Nick en una escapada inolvidable.
-                        </p>
-                    </div>
-                </div>
-            </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </>
     );
 };
