e3fe76326c2f200bf71dc8ca3579837b614734ab
Jonas Abarca Ponce <69958816+jonasabarcaponce@users.noreply.github.com>
2024-11-12 13:00:07 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,11 +8,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     try {
 
-        $to = 'ventas@hotelnickrivieramaya.com'; // Replace with your actual cPanel email
+        $to = 'ventas@hotelnickriviera.com'; // Replace with your actual cPanel email
         $subject = 'Nuevo Lead';
         $body = "Nombre: $name\nCorreo: $email\nTeléfono: $phone";
         
-        $headers = "From: ventas@hotelnickrivieramaya.com\r\n"; // Replace with a valid "From" email
+        $headers = "From: ventas@hotelnickriviera.com\r\n"; // Replace with a valid "From" email
         $headers .= "Reply-To: $email\r\n";
         $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
     
